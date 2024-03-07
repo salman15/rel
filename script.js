b046abbb58b8e392fb1cbf80c17f32371da1e935
Salman <salmannova@gmail.com>
2024-03-07 11:13:26 +0100

    feat: added word groups and comma modfier

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+'use strict';
+
+const below10 = ['null', 'een', 'twee', 'drie', 'vier', 'vijf', 'zes', 'zeven', 'acht', 'negen'];
+const below20 = ['tien', 'elf', 'twaalf', 'dertien', 'veertien', 'vijftien', 'zestien', 'zeventien', 'achttien', 'negentien'];
+const below100 = ['null', 'tien', 'twintig', 'dertig', 'veertig', 'vijftig', 'zestig', 'zeventig', 'tachtig', 'negentig'];
+const below1000 = ['null', 'honderd', 'duizend', 'miljoen', 'miljard']
+
+
 function numberToWords(number) {
-    return number;
+    const numberAsString = String(number);
+    if(numberAsString.length > 13) return 'Number is too large';
+    const minusModifier = number < 0 ? 'min' : '';
+
+    return `${minusModifier}${number}`;
 }
 
 document.getElementById('number-to-words-button').addEventListener('click',function() {
     const number = document.getElementById('number-input').value;
-    const words = numberToWords(number);
-    document.getElementById('number-in-words').innerHTML = words;
+    if(!number) return alert('Please enter a number');
+    const hasComma = number.includes('.');
+    const splitByComma = number.split('.');
+    const words = numberToWords(splitByComma[0]);
+    const wordsAfterComma = hasComma ? ` comma ${numberToWords(splitByComma[1])}` : '';
+    document.getElementById('number-to-words-result').innerText = `Result: ${words}${wordsAfterComma}`;
 })
